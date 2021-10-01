d3eb68e40687042a2395ac11800ce2a67f2a3a49
yingtingl7626 <91745114+yingtingl7626@users.noreply.github.com>
2021-10-01 11:32:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "hannali"
+let userName = "yingtingl7626";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
